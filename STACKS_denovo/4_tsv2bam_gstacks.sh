bf1762568ad25abeb831d916f25daf66331b2635
Jana Wold <jrw0279@gmail.com>
2021-09-09 12:46:39 +1200

    Update 4_tsv2bam_gstacks.sh

--- STACKS_denovo/4_tsv2bam_gstacks.sh
+++ STACKS_denovo/4_tsv2bam_gstacks.sh
@@ -1,157 +1,82 @@
 #!/bin/bash
- 
 #########################################################################################
-#So far we have run ustacks > cstacks > sstacks
-#Now running tsv2bam and gstacks on de novo data
+# So far we have run ustacks > cstacks > sstacks. Now running tsv2bam and gstacks on
+# de novo data
 #########################################################################################
-
 N_pop=/data/Tbulleri/popmaps/Northern_popmap
 S_pop=/data/Tbulleri/popmaps/Southern_popmap
 C_pop=/data/Tbulleri/popmaps/Colonies_popmap
+N_input=/data/Tbulleri/cstacks/Northern/
+S_input=/data/Tbulleri/cstacks/Southern/
+C_input=/data/Tbulleri/cstacks/Colonies/
 
-for file in /data/Tbulleri/cstacks/ustacksM1m*
+for M in {1..8}
     do
-    base=$(basename $file)
-    echo "Working on $base"
-
-    N1=/data/Tbulleri/cstacks/$base/cstacks_n1/Northern
-    S1=/data/Tbulleri/cstacks/$base/cstacks_n1/Southern
-    C1=/data/Tbulleri/cstacks/$base/cstacks_n1/Colonies
-    N2=/data/Tbulleri/cstacks/$base/cstacks_n2/Northern
-    S2=/data/Tbulleri/cstacks/$base/cstacks_n2/Southern
-    C2=/data/Tbulleri/cstacks/$base/cstacks_n2/Colonies
-
-    tsv2bam -P $N1 -M $N_pop -t 30
-    echo "Finished calculating tsv2bam for $N1"
-    tsv2bam -P $S1 -M $S_pop -t 30
-    echo "Finished calculating tsv2bam for $S1"
-    tsv2bam -P $C1 -M $C_pop -t 30
-    echo "Finished calculating tsv2bam for $C1"
-
-    tsv2bam -P $N2 -M $N_pop -t 30
-    echo "Finished calculating tsv2bam for $N2"
-    tsv2bam -P $S2 -M $S_pop -t 30
-    echo "Finished calculating tsv2bam for $S2"
-    tsv2bam -P $C2 -M $C_pop -t 30
-    echo "Finished calculating tsv2bam for $C2"
-
-    gstacks -P $N1 -M $N_pop -t 30
-    echo "Finished calculating gstacks for $N1"
-    gstacks -P $S1 -M $S_pop -t 30
-    echo "Finished calculating gstacks for $S1"
-    gstacks -P $C1 -M $C_pop -t 30
-    echo "Finished calculating gstacks for $C1"
-
-    gstacks -P $N2 -M $N_pop -t 30
-    echo "Finished calculating gstacks for $N2"
-    gstacks -P $S2 -M $S_pop -t 30
-    echo "Finished calculating gstacks for $S2"
-    gstacks -P $C2 -M $C_pop -t 30
-    echo "Finished calculating gstacks for $C2"
+    for j in {3..7}
+        do
+        if ((${M} == 1))
+            then
+            c=${M}
+            c_hi=$((${M} + 1))
+            echo "Running tsv2bam for Northern ustacksM${M}m${j}..."
+            tsv2bam -P ${N_input}ustacksM${M}m${j}_c${c} -M ${N_pop} -t 30 &
+            tsv2bam -P ${N_input}ustacksM${M}m${j}_c${c_hi} -M ${N_pop} -t 30
+            wait
+            echo "Running tsv2bam for Southern ustacksM${M}m${j}..."
+            tsv2bam -P ${S_input}ustacksM${M}m${j}_c${c} -M ${S_pop} -t 30 &
+            tsv2bam -P ${S_input}ustacksM${M}m${j}_c${c_hi} -M ${S_pop} -t 30
+            wait
+            echo "Running tsv2bam for Colonies ustacksM${M}m${j}..."
+            tsv2bam -P ${C_input}ustacksM${M}m${j}_c${c} -M ${C_pop} -t 30 &
+            tsv2bam -P ${C_input}ustacksM${M}m${j}_c${c_hi} -M ${C_pop} -t 30
+            wait
+            echo "Running gstacks for Northern ustacksM${M}m${j}..."
+            gstacks -P ${N_input}ustacksM${M}m${j}_c${c} -M ${N_pop} -t 20 &
+            gstacks -P ${N_input}ustacksM${M}m${j}_c${c_hi} -M ${N_pop} -t 20
+            wait
+            echo "Running gstacks for Southern ustacksM${M}m${j}..."
+            gstacks -P ${S_input}ustacksM${M}m${j}_c${c} -M ${S_pop} -t 20 &
+            gstacks -P ${S_input}ustacksM${M}m${j}_c${c_hi} -M ${S_pop} -t 20
+            wait
+            echo "Running gstacks for Colonies ustacksM${M}m${j}..."
+            gstacks -P ${C_input}ustacksM${M}m${j}_c${c} -M ${C_pop} -t 20 &
+            gstacks -P ${C_input}ustacksM${M}m${j}_c${c_hi} -M ${C_pop} -t 20
+            wait
+        elif ((${M} > 1))
+            then
+            c_low=$((${M} - 1))
+            c=${M}
+            c_hi=$((${M} + 1))
+            echo "Running tsv2bam for Northern ustacksM${M}m${j}..."
+            tsv2bam -P ${N_input}ustacksM${M}m${j}_c${c_low} -M ${N_pop} -t 30 &
+            tsv2bam -P ${N_input}ustacksM${M}m${j}_c${c} -M ${N_pop} -t 30 &
+            tsv2bam -P ${N_input}ustacksM${M}m${j}_c${c_hi} -M ${N_pop} -t 30
+            wait
+            echo "Running tsv2bam for Southern ustacksM${M}m${j}..."
+            tsv2bam -P ${S_input}ustacksM${M}m${j}_c${c_low} -M ${S_pop} -t 30 &
+            tsv2bam -P ${S_input}ustacksM${M}m${j}_c${c} -M ${S_pop} -t 30 &
+            tsv2bam -P ${S_input}ustacksM${M}m${j}_c${c_hi} -M ${S_pop} -t 30
+            wait
+            echo "Running tsv2bam for Colonies ustacksM${M}m${j}..."
+            tsv2bam -P ${C_input}ustacksM${M}m${j}_c${c_low} -M ${C_pop} -t 30 &
+            tsv2bam -P ${C_input}ustacksM${M}m${j}_c${c} -M ${C_pop} -t 30 &
+            tsv2bam -P ${C_input}ustacksM${M}m${j}_c${c_hi} -M ${C_pop} -t 30
+            wait
+            echo "Running gstacks for Northern ustacksM${M}m${j}..."
+            gstacks -P ${N_input}ustacksM${M}m${j}_c${c_low} -M ${N_pop} -t 20 &
+            gstacks -P ${N_input}ustacksM${M}m${j}_c${c} -M ${N_pop} -t 20 &
+            gstacks -P ${N_input}ustacksM${M}m${j}_c${c_hi} -M ${N_pop} -t 20
+            wait
+            echo "Running gstacks for Southern ustacksM${M}m${j}..."
+            gstacks -P ${S_input}ustacksM${M}m${j}_c${c_low} -M ${S_pop} -t 20 &
+            gstacks -P ${S_input}ustacksM${M}m${j}_c${c} -M ${S_pop} -t 20 &
+            gstacks -P ${S_input}ustacksM${M}m${j}_c${c_hi} -M ${S_pop} -t 20
+            wait
+            echo "Running gstacks for Colonies ustacksM${M}m${j}..."
+            gstacks -P ${C_input}ustacksM${M}m${j}_c${c_low} -M ${C_pop} -t 20 &
+            gstacks -P ${C_input}ustacksM${M}m${j}_c${c} -M ${C_pop} -t 20 &
+            gstacks -P ${C_input}ustacksM${M}m${j}_c${c_hi} -M ${C_pop} -t 20
+            wait
+        fi
+    done
 done
-
-#########################################################################################
-
-#for file in /data/Tbulleri/cstacks/ustacksM2m*
-#    do
-#    base=$(basename $file)
-#    echo "Working on $base"
-#
-#    N1=/data/Tbulleri/cstacks/$base/cstacks_n1/Northern
-#    S1=/data/Tbulleri/cstacks/$base/cstacks_n1/Southern
-#    C1=/data/Tbulleri/cstacks/$base/cstacks_n1/Colonies
-#    N2=/data/Tbulleri/cstacks/$base/cstacks_n2/Northern
-#    S2=/data/Tbulleri/cstacks/$base/cstacks_n2/Southern
-#    C2=/data/Tbulleri/cstacks/$base/cstacks_n2/Colonies
-#    N3=/data/Tbulleri/cstacks/$base/cstacks_n3/Northern
-#    S3=/data/Tbulleri/cstacks/$base/cstacks_n3/Southern
-#    C3=/data/Tbulleri/cstacks/$base/cstacks_n3/Colonies
-#
-#    tsv2bam -P $N1 -M $N_pop -t 30
-#    echo "Finished calculating tsv2bam for $N1"
-#    tsv2bam -P $S1 -M $S_pop -t 30
-#    echo "Finished calculating tsv2bam for $S1"
-#    tsv2bam -P $C1 -M $C_pop -t 30
-#    echo "Finished calculating tsv2bam for $C1"
-#
-#    tsv2bam -P $N2 -M $N_pop -t 30
-#    echo "Finished calculating tsv2bam for $N2"
-#    tsv2bam -P $S2 -M $S_pop -t 30
-#    echo "Finished calculating tsv2bam for $S2"
-#    tsv2bam -P $C2 -M $C_pop -t 30
-#    echo "Finished calculating tsv2bam for $C2"
-#
-#    gstacks -P $N1 -M $N_pop -t 30
-#    echo "Finished calculating gstacks for $N1"
-#    gstacks -P $S1 -M $S_pop -t 30
-#    echo "Finished calculating gstacks for $S1"
-#    gstacks -P $C1 -M $C_pop -t 30
-#    echo "Finished calculating gstacks for $C1"
-#
-#    gstacks -P $N2 -M $N_pop -t 30
-#    echo "Finished calculating gstacks for $N2"
-#    gstacks -P $S2 -M $S_pop -t 30
-#    echo "Finished calculating gstacks for $S2"
-#    gstacks -P $C2 -M $C_pop -t 30
-#    echo "Finished calculating gstacks for $C2"
-#done
-
-#########################################################################################
-
-#for file in /data/Tbulleri/cstacks/ustacksM3m*
-#    do
-#    base=$(basename $file)
-#    echo "Working on $base"
-#
-#    N1=/data/Tbulleri/cstacks/$base/cstacks_n2/Northern
-#    S1=/data/Tbulleri/cstacks/$base/cstacks_n2/Southern
-#    C1=/data/Tbulleri/cstacks/$base/cstacks_n2/Colonies
-#    N2=/data/Tbulleri/cstacks/$base/cstacks_n3/Northern
-#    S2=/data/Tbulleri/cstacks/$base/cstacks_n3/Southern
-#    C2=/data/Tbulleri/cstacks/$base/cstacks_n3/Colonies
-#    N3=/data/Tbulleri/cstacks/$base/cstacks_n4/Northern
-#    S3=/data/Tbulleri/cstacks/$base/cstacks_n4/Southern
-#    C3=/data/Tbulleri/cstacks/$base/cstacks_n4/Colonies
-#
-#    tsv2bam -P $N1 -M $N_pop -t 30
-#    echo "Finished calculating tsv2bam for $N1"
-#    tsv2bam -P $S1 -M $S_pop -t 30
-#    echo "Finished calculating tsv2bam for $S1"
-#    tsv2bam -P $C1 -M $C_pop -t 30
-#    echo "Finished calculating tsv2bam for $C1"
-#
-#    tsv2bam -P $N2 -M $N_pop -t 30
-#    echo "Finished calculating tsv2bam for $N2"
-#    tsv2bam -P $S2 -M $S_pop -t 30
-#    echo "Finished calculating tsv2bam for $S2"
-#    tsv2bam -P $C2 -M $C_pop -t 30
-#    echo "Finished calculating tsv2bam for $C2"
-#
-#    tsv2bam -P $N3 -M $N_pop -t 30
-#    echo "Finished calculating tsv2bam for $N3"
-#    tsv2bam -P $S3 -M $S_pop -t 30
-#    echo "Finished calculating tsv2bam for $S3"
-#    tsv2bam -P $C3 -M $C_pop -t 30
-#    echo "Finished calculating tsv2bam for $C3"
-#
-#    gstacks -P $N1 -M $N_pop -t 30
-#    echo "Finished calculating gstacks for $N1"
-#    gstacks -P $S1 -M $S_pop -t 30
-#    echo "Finished calculating gstacks for $S1"
-#    gstacks -P $C1 -M $C_pop -t 30
-#    echo "Finished calculating gstacks for $C1"
-#
-#    gstacks -P $N2 -M $N_pop -t 30
-#    echo "Finished calculating gstacks for $N2"
-#    gstacks -P $S2 -M $S_pop -t 30
-#    echo "Finished calculating gstacks for $S2"
-#    gstacks -P $C2 -M $C_pop -t 30
-#    echo "Finished calculating gstacks for $C2"
-#
-#    gstacks -P $N3 -M $N_pop -t 30
-#    echo "Finished calculating gstacks for $N3"
-#    gstacks -P $S3 -M $S_pop -t 30
-#    echo "Finished calculating gstacks for $S3"
-#    gstacks -P $C3 -M $C_pop -t 30
-#    echo "Finished calculating gstacks for $C3"
-#done
